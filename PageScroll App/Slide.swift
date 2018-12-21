90395b80b1275cb2a0f42a18964e779731865647
Mustafa <mq.kakajiwala@gmail.com>
2018-12-21 11:40:59 +0500

    Scroll Images using page control and UIScrollView

---
+++ PageScroll App/Slide.swift
@@ -0,0 +1,16 @@
+//
+//  Slide.swift
+//  PageScroll App
+//
+//  Created by Mustafa on 21/12/2018.
+//  Copyright © 2018 Mustafa. All rights reserved.
+//
+
+import UIKit
+
+class Slide: UIView {
+
+    @IBOutlet weak var slidingImageView: UIImageView!
+    
+
+}
