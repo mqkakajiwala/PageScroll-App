4a915563a96116b4823656c5d8350a4622871f78
Mustafa <mq.kakajiwala@gmail.com>
2018-12-21 10:38:37 +0500

    Initial Commit

---
+++ PageScroll App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PageScroll App
+//
+//  Created by Mustafa on 21/12/2018.
+//  Copyright © 2018 Mustafa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
